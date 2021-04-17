09c14bc8e2d25d91f030abb6e04e5cec6cbb0c1d
marleigh <volcana@gmail.com>
2021-04-16 18:35:21 -0700

    fun with css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.getElementById('hamburger-menu');
 const nav = document.querySelector(".nav");
+const header = document.getElementById('header');
 
 hamburger.addEventListener('click', e => {
     nav.classList.toggle('show');
+    //header.classList.toggle('bg-color');
+
 });
