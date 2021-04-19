e2e1b618d4f6f5c47973110dea82a981311ea827
marleigh <volcana@gmail.com>
2021-04-19 16:30:16 -0700

    working on a hover effect

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const hamburger = document.getElementById('hamburger-menu');
 const nav = document.querySelector(".nav");
 const header = document.getElementById('header');
+const links = document.querySelectorAll('.main__projects__grid--link');
 
-hamburger.addEventListener('click', e => {
-    nav.classList.toggle('show');
-    //header.classList.toggle('bg-color');
+hamburger.addEventListener('click', () => nav.classList.toggle('show'));
 
+links.forEach(link => {
+    link.addEventListener('mouseOver', imageHover); //i don't think this is hover exactly
 });
+
+function imageHover() {
+    //main__projects__grid--link 
+    console.log('test');
+
+}
+
+imageHover();
+/*
+need to create these elements with their copy - somehow get it to activate on hover
+                    <h6 class="main__projects__grid--desc-title">Project #1</h6>
+                    <p class="main__projects__grid--desc">short description??</p>
+
+*/
