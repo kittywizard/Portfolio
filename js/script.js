94b603172a76c4a7608ce2e4e159a49e898ed76e
marleigh <volcana@gmail.com>
2021-04-28 14:29:09 -0700

    updating the background

--- js/script.js
+++ js/script.js
@@ -3,19 +3,28 @@ const nav = document.querySelector(".nav");
 const header = document.getElementById('header');
 const links = document.querySelectorAll('.main__projects__grid--link');
 
-hamburger.addEventListener('click', () => nav.classList.toggle('show'));
+hamburger.addEventListener('click', () => nav.classList.toggle('show')); // for the menu to work
 
 links.forEach(link => {
-    link.addEventListener('click', imageHover); //i don't think this is hover exactly
+    link.addEventListener('click', imageHover); 
 });
 
 function imageHover() {
-    //main__projects__grid--link 
-    console.log('test');
+
+    //what to do on image hover/click
+
+    // create a modal? pop up a larger image + descriptor info
+
 
 }
 
 imageHover();
+
+function createModal() {
+
+    let modal = document.createElement('div');
+    
+}
 /*
 need to create these elements with their copy - somehow get it to activate on hover
                     <h6 class="main__projects__grid--desc-title">Project #1</h6>
