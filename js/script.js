c5e6615d155b7502e1fe5bdf09700961b525ca31
marleigh <volcana@gmail.com>
2021-05-10 15:18:37 -0700

    nav tweaks - animation and closing via link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
+//click the hamburger, get a menu
 const hamburger = document.getElementById('hamburger-menu');
-hamburger.addEventListener('click', () => nav.classList.toggle('nav-open')); // for the menu to work
+const nav = document.querySelector('.nav');
+hamburger.addEventListener('click', closeNav); // for the menu to work
+
+//close the navigation when you click any of the links
+const navLinks = document.querySelectorAll('.nav__link');
+console.log(navLinks);
+navLinks.forEach(link => {
+    link.addEventListener('click', closeNav);
+});
+
+function closeNav() {
+    nav.classList.toggle('nav-open');
+}
 
 //need to create the hover effect
 const links = document.querySelectorAll('.main__projects__grid--link');
@@ -9,6 +22,4 @@ links.forEach(link => {
         //function
     }); 
 });
-
-
 
