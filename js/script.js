50dfd42cc4b7a07bcf76a7cdd56b99f526bf16e3
marleigh <volcana@gmail.com>
2021-05-19 11:50:39 -0700

    adding light mode classes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 //click the hamburger, get a menu
 const hamburger = document.getElementById('hamburger-menu');
-const nav = document.querySelector('.nav');
-const main = document.querySelector("main");
 hamburger.addEventListener('click', closeNav); // for the menu to work
 
 //close the navigation when you click any of the links
@@ -15,16 +13,21 @@ function closeNav() {
     hamburger.classList.toggle('rotate');
 }
 
-//need to create the hover effect
-const links = document.querySelectorAll('.main__projects__grid--link');
-const desc = document.querySelectorAll('.main__projects__grid--desc');
-links.forEach((link, index) => {
-    link.addEventListener('click', () => {
+//gathering all the necessary elements to change classes on
+const nav = document.querySelector('.nav');
+const main = document.querySelector("main");
+const header = document.getElementById('header');
 
-       desc[index].classList.toggle('hidden');
-       links[index].classList.add('absolute');
-    }); 
-});
+//need to create the hover effect
+// const links = document.querySelectorAll('.main__projects__grid--link');
+// const desc = document.querySelectorAll('.main__projects__grid--desc');
+// links.forEach((link, index) => {
+//     link.addEventListener('click', () => {
+
+//        desc[index].classList.toggle('hidden');
+//        links[index].classList.add('absolute');
+//     }); 
+// });
 
 const toggleBtn = document.getElementById('nav__toggle-btn');
 toggleBtn.addEventListener('click', changeMode);
@@ -36,8 +39,15 @@ function changeMode() {
     toggleBtn.classList.toggle('toggled');
     lighten.classList.toggle('unselected');
     darken.classList.toggle('unselected');
-
-    //need to reassign css colors
-    //probably should remove color functionality from main classes
     
+    if(lighten.classList.contains('unselected')) {
+
+    } else {
+
+    }
+}
+
+function switchClass(element, classRemove, classAdd) {
+    element.classList.remove(classRemove);
+    element.classList.add(classAdd);
 }
